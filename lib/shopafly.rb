7aff1256f3758fc072097e0281fe7bbf10e8bc22
IamNaN <dgerton@gmail.com>
2021-03-28 15:16:58 -0600

    initializing repository

---
+++ lib/shopafly.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "shopafly/version"
+
+module Shopafly
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/shopafly/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Shopafly
+  VERSION = "0.1.0"
+end
